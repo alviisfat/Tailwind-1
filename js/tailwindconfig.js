beb0afc7390170974764fee2493e31303a533bed
isfathossainalvi <isfathossainalvi58@gmail.com>
2023-11-10 14:29:58 -0800

    first commit

---
+++ js/tailwindconfig.js
@@ -0,0 +1,68 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        // Hamara Code Starts
+        // Header Color Code Starts
+        colors: {
+          headerColor: "rgba(22, 13, 110, 0.50)",
+          btnbg: "#1BBF00",
+          hovercolor: "#1BBF00",
+          overlycolor: "hsla(245, 46%, 58%, 0.9)",
+          onekcolor: "#1BBF00",
+          bordercolor: "hsla(207, 16%, 73%, 0.623)",
+          textcol: "#141135",
+          Collor: "#F9F9FB",
+          cilor: "#726E9E",
+          white: "#ffffff",
+          whatwedo: "#F6F5FF",
+          coltextt: "#726E9E",
+          box: "#6B62C5",
+          pcol: "#FAFAFE",
+          colorpack: "#F9F9FB",
+          col: "#141135",
+          col2: "#1BBF00",
+          col3: "#726E9E",
+          col4: "#6B62C5",
+          col5: "#8981D1",
+          col6: "221A2C",
+          col7: "#1BBF00",
+          col8: "#726E9E",
+          footer: "#48409C",
+
+        },
+        // Header Color Code Ends
+        // All Container Starts
+        width: {
+            headercontainer: "1170px",
+            bannercontainer: "962px",
+            Impactcontainer: "1080px",
+            likecontainer: "1920px",
+        },
+         // All Container Ends
+
+         // All Background Images code Starts
+         backgroundImage: {
+          bannerbg: "url('images/banner.png')",
+          analysisbg: "url('images/analysisbg.png')",
+          choosebg: "url('images/choosebg.png')",
+         },
+         // All Background Images code Ends
+
+         // Font Family Starts
+         fontFamily: {
+          opens: ["Open Sans", "sans-serif"],
+          paprika: ["Paprika", "cursive"],
+          quicksand: ["Quicksand", "sans-serif"],
+         },
+         // Font Family Ends
+         // box shadow starts
+         boxShadow: {
+           choosebox: '10px 30px 60px 0px rgba(162, 169, 192, 0.15)',
+           
+         },
+         // box shadow ends
+        // Hamara Code Ends
+         }
+      }
+    }
+  
